30e7b5dbfeb59e9bde8d300e43ae0a7565c9e34a
marco <kumawatmohit977@gmail.com>
2024-07-05 04:49:00 +0000

    second time updated code

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get('/',(req,res)=>{
+  res.send('Hello World')
+})
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
